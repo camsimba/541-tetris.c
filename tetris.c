d99fb6dc48e1cd4061b744745bd120d8abb56de2
Cameron Simbeck <camsimba@ad.unc.edu>
2021-11-15 23:30:38 -0500

    bad rot

--- tetris.c
+++ tetris.c
@@ -14,6 +14,7 @@ int height = 20;
 struct Block {
     int x;
     int y;
+    int rot;
 };
 
 int gameState[200] = {0};
@@ -77,6 +78,23 @@ bool check_right(){
     return true;
 }
 
+bool check_block(){
+    if(b1.x < 0 || b1.x >= width || b2.x < 0 || b2.x >= width || b3.x < 0 || b3.x >= width || b4.x < 0 || b4.x >= width){
+        return false;
+    }
+    if(b1.y < 0 || b1.y >= height || b2.y < 0 || b2.y >= height || b3.y < 0 || b3.y >= height || b4.y < 0 || b4.y >= height){
+        return false;
+    }
+    int newb1 = gameState[b1.x + width*b1.y];
+    int newb2 = gameState[b2.x + width*b2.y];
+    int newb3 = gameState[b3.x + width*b3.y];
+    int newb4 = gameState[b4.x + width*b4.y];
+    if(newb1 != 0 || newb2 != 0 || newb3 != 0 || newb4 != 0){
+        return false;
+    }
+    return true;
+}
+
 void drawGame(){
     printf("%c", '\n');
     printf("----------------------------------------");
@@ -94,7 +112,7 @@ void drawGame(){
     return;
 }
 
-//updates current falling block; 1 = stay on current block / -1 = game over / 0 = switch to new block
+//updates current falling block; return 1 = stay on current block / 0 = switch to new block
 int updateBlock(int dir){
     //dir --> 0 = down; 1 = left; 2 = right;
     removeBlocksFromState();
@@ -149,11 +167,10 @@ bool placeBlock(int block){
             b4.x = 5; b4.y = 2;
             break;
         case 3: //I
-            b1.x,b2.x,b3.x,b4.x = 4;
-            b1.y = 0;
-            b2.y = 1;
-            b3.y = 2;
-            b4.y = 3;
+            b1.x = 4; b1.y = 0;
+            b2.x = 4; b2.y = 1;
+            b3.x = 4; b3.y = 2;
+            b4.x = 4; b4.y = 3;
             break;
         case 4: //SQ
             b1.x = 4; b1.y = 0;
@@ -174,6 +191,7 @@ bool placeBlock(int block){
             b4.x = 5; b4.y = 2;
             break;
     }
+    b1.rot = 0; b2.rot = 0; b3.rot = 0; b4.rot = 0;
     int newb1 = gameState[b1.x + width*b1.y];
     int newb2 = gameState[b2.x + width*b2.y];
     int newb3 = gameState[b3.x + width*b3.y];
@@ -185,8 +203,170 @@ bool placeBlock(int block){
     return true;
 }
 
+bool rotateBlock(int block){
+    struct Block tb1, tb2, tb3, tb4;
+    memcpy(&b1, &tb1, sizeof b1);
+    memcpy(&b2, &tb2, sizeof b2);
+    memcpy(&b3, &tb3, sizeof b3);
+    memcpy(&b4, &tb4, sizeof b4);
+    switch(block){
+        case 0: //T
+            switch(b2.rot){
+                case 0:
+                    b4.x = b2.x;
+                    b4.y = b2.y - 1;
+                    break;
+                case 1:
+                    b3.x = b2.x + 1;
+                    b3.y = b2.y;
+                    break;
+                case 2:
+                    b1.x = b2.x;
+                    b1.y = b2.y + 1;
+                    break;
+                case 3:
+                    b1.x = b2.x-1;
+                    b1.y = b2.y;
+                    b3.x = b2.x;
+                    b3.y = b2.y+1;
+                    b4.x = b2.x+1;
+                    b4.y = b2.y;
+                    break;
+            }
+            break;
+        case 1: //S
+            switch(b2.rot){
+                case 0:
+                    b4.y += 1;
+                    b1.x = b4.x; b1.y = b4.y + 1;
+                    break;
+                case 1:
+                    b4.y -= 1;
+                    b1.x = b2.x - 1; b1.y = b2.y;
+                    break;
+                case 2:
+                    b4.y += 1;
+                    b1.x = b4.x; b1.y = b4.y + 1;
+                    break;
+                case 3:
+                    b4.y -= 1;
+                    b1.x = b2.x - 1; b1.y = b2.y;
+                    break;
+            }
+        case 2: //L
+            switch(b2.rot){
+                case 0:
+                    b1.x = b2.x + 1; b1.y = b2.y;
+                    b3.x = b2.x - 1; b3.y = b2.y;
+                    b4.x = b3.x; b4.y = b2.y + 1;
+                    break;
+                case 1:
+                    b1.x = b2.x; b1.y = b2.y + 1;
+                    b3.x = b2.x; b3.y = b2.y - 1;
+                    b4.x = b3.x - 1; b4.y = b3.y;
+                    break;
+                case 2:
+                    b1.x = b2.x -1; b1.y = b2.y;
+                    b3.x = b2.x + 1; b3.y = b2.y;
+                    b4.x = b3.x; b4.y = b3.y -1;
+                    break;
+                case 3:
+                    b1.x = b2.x; b1.y = b2.y - 1;
+                    b3.x = b2.x; b3.y = b2.y + 1;
+                    b4.x = b2.x + 1; b4.y = b3.y;
+                    break;
+            }
+            break;
+        case 3: //I
+            switch(b2.rot){
+                case 0:
+                    b1.x = b2.x - 1; b1.y = b2.y;
+                    b3.x = b2.x + 1; b3.y = b2.y;
+                    b4.x = b2.x + 2; b4.y = b2.y;
+                    break;
+                case 1:
+                    b1.x = b2.x; b1.y = b2.y - 1;
+                    b3.x = b2.x; b3.y = b2.y + 1;
+                    b4.x = b2.x; b4.y = b2.y + 2;
+                    break;
+                case 2:
+                    b1.x = b2.x - 1; b1.y = b2.y;
+                    b3.x = b2.x + 1; b3.y = b2.y;
+                    b4.x = b2.x + 2; b4.y = b2.y;
+                    break;
+                case 3:
+                    b1.x = b2.x; b1.y = b2.y - 1;
+                    b3.x = b2.x; b3.y = b2.y + 1;
+                    b4.x = b2.x; b4.y = b2.y + 2;
+                    break;
+            }
+            break;
+        case 4: //SQ
+            break;
+        case 5: //Z
+            switch(b2.rot){
+                case 0:
+                    b1.x = b2.x; b1.y = b2.y - 1;
+                    b3.x = b2.x - 1; b3.y = b2.y;
+                    b4.x = b3.x; b4.y = b2.y + 1;
+                    break;
+                case 1:
+                    b1.x = b2.x + 1; b1.y = b2.y;
+                    b3.x = b2.x; b3.y = b2.y - 1;
+                    b4.x = b2.x - 1; b4.y = b3.y;
+                    break;
+                case 2:
+                    b1.x = b2.x; b1.y = b2.y + 1;
+                    b3.x = b2.x + 1; b3.y = b2.y;
+                    b4.x = b3.x; b4.y = b2.y - 1;
+                    break;
+                case 3:
+                    b1.x = b2.x - 1; b1.y = b2.y;
+                    b3.x = b2.x; b3.y = b2.y + 1;
+                    b4.x = b2.x + 1; b4.y = b3.y;
+                    break;
+            }
+            break;
+        case 6: //BL
+            switch(b2.rot){
+                case 0:
+                    b1.x = b2.x + 1; b1.y = b2.y;
+                    b3.x = b2.x - 1; b3.y = b2.y;
+                    b4.x = b3.x; b4.y = b2.y - 1;
+                    break;
+                case 1:
+                    b1.x = b2.x; b1.y = b2.y + 1;
+                    b3.x = b2.x; b3.y = b2.y - 1;
+                    b4.x = b2.x + 1; b4.y = b3.y;
+                    break;
+                case 2:
+                    b1.x = b2.x - 1; b1.y = b2.y;
+                    b3.x = b2.x + 1; b3.y = b2.y;
+                    b4.x = b3.x; b4.y = b2.y + 1;
+                    break;
+                case 3:
+                    b1.x = b2.x; b1.y = b2.y - 1;
+                    b3.x = b2.x; b3.y = b2.y + 1;
+                    b4.x = b2.x - 1; b4.y = b3.y;
+                    break;
+            }
+            break;
+    }
+    if(!check_block()){
+        memcpy(&tb1, &b1, sizeof b1);
+        memcpy(&tb2, &b2, sizeof b2);
+        memcpy(&tb3, &b3, sizeof b3);
+        memcpy(&tb4, &b4, sizeof b4);
+        return false;
+    }
+    b1.rot = (b1.rot + 1) % 4; b2.rot = (b2.rot + 1) % 4; b3.rot = (b3.rot + 1) % 4; b4.rot = (b4.rot + 1) % 4;
+    removeBlocksFromState();
+    updateState();
+    return true;
+}
+
 bool game_start(){
-    int randBlock, randRot;
+    int randBlock;
     int currBlock = 0;
     while(1){
         drawGame();
@@ -200,22 +380,19 @@ bool game_start(){
             dir = 0;
         }else if(chr == 'd'){
             dir = 2;
+        }else if(chr == 'w'){
+            rotateBlock(randBlock);
         }else{
             dir = 0;
         }
         if(currBlock == 0){
             randBlock = rand() % 7;
-            //randRot = rand() % 4;
             if(!placeBlock(randBlock)){
                 printf("game over");
                 break;
             }
         }
         currBlock = updateBlock(dir);
-        if(currBlock == -1){
-            printf("game over");
-            break;
-        }
     }
     return true; //exit game
 }
